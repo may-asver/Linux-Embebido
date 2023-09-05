cf0e0ddcb96bf267ba7cb3a5c0749a15606addad
Maya A <si726029@iteso.mx>
2023-09-04 21:03:00 -0600

    Semaphore
    Semaphore reader and writer to PICO-PI-IMX8M

---
+++ Scripts/Semaphore/common.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define SHM_KEY 0xADFF
+#define SEM_WR_NAME "WriteSemaphore"
+#define SEM_RD_NAME "ReadSemaphore"
+
+#endif
---
+++ Scripts/Semaphore/smh_reader.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+#include "common.h"
+
+#define MAX_LEN 80
+
+int main (int argc, char * argv[]) {
+	char * buff;
+	int ret;
+	
+	int shm_id = shmget(SHM_KEY, 4096, IPC_CREAT | 0666);
+	if (shm_id == -1)
+		exit(-1);
+	buff = shmat(shm_id, 0, 0);
+	sem_t * sem_wr = sem_open(SEM_WR_NAME, O_CREAT, 0666);
+	if(sem_wr == NULL)
+		exit(-1);
+		
+	for (;;) {
+		int ret = sem_wait(sem_wr);
+		printf("%s",buff);
+		
+	}
+	ret = sem_unlink(SEM_WR_NAME);
+	ret = shmdt(buff);
+	if (ret == 0)
+		printf("Memory dettached\n");
+	
+	return 0;
+}
